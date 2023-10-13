8857cc5500e1beb4e0ca0451eed861f819cfcbb2
Riju Mondal <pilotrijurocks@gmail.com>
2023-10-13 16:08:14 +0530

    update text hover

--- src/components/texts.jsx
+++ src/components/texts.jsx
@@ -1,10 +1,17 @@
 import { Text } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import React, { useRef } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import React, { useRef, useState } from "react";
 
-export const TextSection = ({ sceneOpacity, title, subtitle, ...props }) => {
-   const materialRef = useRef();
-   const titlematerialRef = useRef();
+export const TextSection = ({ sceneOpacity, title, subtitle,  ...props }) => {
+  const materialRef = useRef();
+  const titlematerialRef = useRef();
+  const [hovered, setHover] = useState(false);
+  const { camera } = useThree();
+  const linkURL = "https://www.google.com";
+
+  const handleClick = () => {
+    window.open(linkURL, "_blank"); // Open the link in a new tab/window
+  };
 
   useFrame(() => {
     materialRef.current.opacity = sceneOpacity.current;
@@ -15,33 +22,33 @@ export const TextSection = ({ sceneOpacity, title, subtitle, ...props }) => {
     <>
       <group {...props}>
         <Text
-          color="white"
+          color={hovered ? "blue" : "white"}
           anchorX="right"
           anchorY="bottom"
           fontSize={1}
           maxWidth={3.5}
           font="/Fonts/Fuggles/Fuggles-Regular.ttf"
+          onPointerOver={() => setHover(true)}
+          onPointerOut={() => setHover(false)}
+          onClick={handleClick}
         >
           {title}
-        <meshStandardMaterial
-           ref={titlematerialRef}
-          
-        />
+          <meshStandardMaterial ref={titlematerialRef} />
         </Text>
 
         <Text
-          color="white"
+          color={hovered ? "blue" : "white"}
           anchorX="right"
           anchorY="top"
           fontSize={0.7}
           maxWidth={3.5}
           font="/Fonts/Fuggles/Fuggles-Regular.ttf"
+          onPointerOver={() => setHover(true)}
+          onPointerOut={() => setHover(false)}
+          onClick={handleClick}
         >
           {subtitle}
-        <meshStandardMaterial
-           ref={materialRef}
-          
-        />
+          <meshStandardMaterial ref={materialRef} />
         </Text>
       </group>
     </>
